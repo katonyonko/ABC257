1589db95a368895c8d54508645bef81e451d25dc
katonyonko <katonyonko@yahoo.co.jp>
2022-06-28 00:04:24 +0900

    Gは解けても良かった気はする

--- ABC257_G.py
+++ ABC257_G.py
@@ -7,11 +7,50 @@ aba
 ababaab
 atcoder
 ac
+atcoder
+ba
 """
 
 sys.stdin = io.StringIO(_INPUT)
 case_no=int(input())
 for __ in range(case_no):
+  #Segment Tree
+  class SegTree:
+      X_unit = 1 << 30
+      X_f = min
+
+      def __init__(self, N):
+          self.N = N
+          self.X = [self.X_unit] * (N + N)
+
+      def build(self, seq):
+          for i, x in enumerate(seq, self.N):
+              self.X[i] = x
+          for i in range(self.N - 1, 0, -1):
+              self.X[i] = self.X_f(self.X[i << 1], self.X[i << 1 | 1])
+
+      def set_val(self, i, x):
+          i += self.N
+          self.X[i] = x
+          while i > 1:
+              i >>= 1
+              self.X[i] = self.X_f(self.X[i << 1], self.X[i << 1 | 1])
+
+      def fold(self, L, R):
+          L += self.N
+          R += self.N
+          vL = self.X_unit
+          vR = self.X_unit
+          while L < R:
+              if L & 1:
+                  vL = self.X_f(vL, self.X[L])
+                  L += 1
+              if R & 1:
+                  R -= 1
+                  vR = self.X_f(self.X[R], vR)
+              L >>= 1
+              R >>= 1
+          return self.X_f(vL, vR)
   def z_algorithm(s):
     N = len(s)
     Z_alg = [0]*N
@@ -35,19 +74,11 @@ for __ in range(case_no):
   S=input()
   T=input()
   tmp=z_algorithm(S+'_'+T)[-len(T):]
-  tmp=[i+tmp[i] for i in range(len(T))]
-  key=set(tmp)
-  d={k:10**10 for k in key}
+  tmp=[tmp[i] for i in range(len(T))]
+  sg=SegTree(len(T)+1)
+  sg.build([10**10 if i <len(T) else 0 for i in range(len(T)+1)])
   for i in range(len(T)):
-    d[tmp[i]]=min(d[tmp[i]],i)
-  key=sorted(list(key),reverse=True)
-  ans=0
-  now=key[0]
-  next=d[key[0]]
-  for i in range(len(key)):
-    if key[i]<now:
-      now=next
-      ans+=1
-    next=min(next,d[key[i]])
-  if ans==0: print(-1)
+    sg.set_val(len(T)-i-1,sg.fold(len(T)-i,len(T)-i+tmp[len(T)-i-1])+1)
+  ans=sg.fold(0,1)
+  if ans==1<< 30: print(-1)
   else: print(ans)
